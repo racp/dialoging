90a579b13b1cacd25eadbb368a5a328d0fd8a16d
rucastellanos <rucastellanos@siptec.com.do>
2018-10-15 16:51:10 -0400

    First pust

---
+++ Dialoging/src/main/java/com/gmail/ruben/spring/MainView.java
@@ -0,0 +1,18 @@
+package com.gmail.ruben.spring;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    
+	private static final long serialVersionUID = 1L;
+
+	private ConfirmDialog confirmDialog;
+	
+	public MainView() {
+       
+    }
+
+}
